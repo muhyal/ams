e3de0175fe333147781f04d58f3220b7a1f60ae0
Muhammed Yalçınkaya <55546452+muhyal@users.noreply.github.com>
2023-08-31 14:16:47 +0300

    Basic template added

--- access_denied.php
+++ access_denied.php
@@ -1,1 +1,48 @@
-Access Denied
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Erişim Reddedildi</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f8f9fa;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+
+        .container {
+            text-align: center;
+        }
+
+        .error-code {
+            font-size: 72px;
+            color: #dc3545;
+            margin-bottom: 20px;
+        }
+
+        .error-message {
+            font-size: 24px;
+            color: #333;
+            margin-bottom: 20px;
+        }
+
+        .back-link {
+            font-size: 18px;
+            color: #007bff;
+            text-decoration: none;
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+    <div class="error-code">403</div>
+    <div class="error-message">Erişim Reddedildi</div>
+    <p>Üzgünüz, bu sayfaya erişim izniniz bulunmuyor.</p>
+    <a class="back-link" href="javascript:history.back()">Geri Dön</a>
+</div>
+</body>
+</html>
