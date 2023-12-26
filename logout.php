d31a267bd192c85a487bb1218d5e24dd9912d482
Muhammed Yalçınkaya <55546452+muhyal@users.noreply.github.com>
2023-12-26 20:38:29 +0300

    oturum kapatılınca bir önceki sayfaya dön

--- logout.php
+++ logout.php
@@ -1,7 +1,19 @@
 <?php
 session_start();
+
+// Önceki sayfanın URL'sini al
+$previousPage = $_SERVER["HTTP_REFERER"];
+
+// Oturumu temizle ve sonlandır
 session_unset();
 session_destroy();
-header("Location: /index.php"); // Giriş sayfasına yönlendir
+
+// Eğer önceki sayfa bilgisi varsa, kullanıcıyı o sayfaya yönlendir
+if (!empty($previousPage)) {
+    header("Location: $previousPage");
+} else {
+    header("Location: /index.php"); // Varsayılan sayfaya yönlendirme
+}
+
 exit();
 ?>
