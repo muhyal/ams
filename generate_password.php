37f1b2ebbeaa248b126455d8d681bf51bf9ef7ef
Muhammed Yalçınkaya <55546452+muhyal@users.noreply.github.com>
2023-12-27 12:11:34 +0300

    user ve admin için şifre üretme fonksiyonu

---
+++ generate_password.php
@@ -0,0 +1,13 @@
+<?php
+function generatePassword($length = 10) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()-_+=<>?';
+    $password = '';
+    for ($i = 0; $i < $length; $i++) {
+        $password .= $characters[rand(0, strlen($characters) - 1)];
+    }
+    echo $password;
+}
+
+// Örneğin, 10 karakter uzunluğunda bir şifre üretelim
+generatePassword(10);
+?>
