ad20ede88a06dcb243bc31a96954ca49515ea98b
Muhammed Yalçınkaya <55546452+muhyal@users.noreply.github.com>
2024-01-02 16:51:07 +0300

    footer eklendi :)

--- footer.php
+++ footer.php
@@ -23,7 +23,19 @@
    </main>
    </div>
    </div>
-   <script src="./assets/dist/js/bootstrap.bundle.min.js"></script>
+<!-- Boş alan -->
+<div style="height: 55px;"></div>
+<!-- Footer -->
+<footer class="footer fixed-bottom bg-body-secondary">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-6 mt-3">
+                <p>&copy; <?php echo (new DateTime())->format('Y') ?>, <?php echo $siteName ?>. Tüm hakları saklıdır.</p>
+            </div>
+        </div>
+    </div>
+</footer>
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
    <script src="https://cdn.jsdelivr.net/npm/chart.js@4.3.2/dist/chart.umd.js" integrity="sha384-eI7PSr3L1XLISH8JdDII5YN/njoSsxfbrkCTnJrzXt+ENP5MOVBxD+l6sEG4zoLp" crossorigin="anonymous"></script>
    <script src="./assets/js/dashboard.js"></script></body>
    </html>
