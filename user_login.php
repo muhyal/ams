e7a9ebdac0daf5a82e4011dc7d6648686d7ef3ba
Muhammed Yalçınkaya <55546452+muhyal@users.noreply.github.com>
2024-01-12 17:46:16 +0300

    login düzenlendi

--- user_login.php
+++ user_login.php
@@ -17,55 +17,94 @@
  *
  * You should have received a copy of the GNU Affero General Public License, version 3,
  * along with this program. If not, see <http://www.gnu.org/licenses/>
- *
  */
-global $siteHeroDescription, $showErrors, $siteName, $siteShortName, $siteUrl;
-// Hata mesajlarını göster veya gizle ve ilgili işlemleri gerçekleştir
-$showErrors ? ini_set('display_errors', 1) : ini_set('display_errors', 0);
-$showErrors ? ini_set('display_startup_errors', 1) : ini_set('display_startup_errors', 0);
-require_once "config.php";
-
+global $db, $showErrors;
 // Oturum kontrolü
 session_start();
 session_regenerate_id(true);
 
+require_once "db_connection.php";
 
-// CSRF token oluşturma veya varsa alınması
-if (!isset($_SESSION['csrf_token'])) {
-    $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+// Hata mesajlarını göster veya gizle ve ilgili işlemleri gerçekleştir
+$showErrors ? ini_set('display_errors', 1) : ini_set('display_errors', 0);
+$showErrors ? ini_set('display_startup_errors', 1) : ini_set('display_startup_errors', 0);
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // CSRF token kontrolü
+    if (!isset($_POST['csrf_request']) || $_POST['csrf_request'] !== '1') {
+        die("CSRF hatası! İşlem reddedildi.");
+    }
+
+    $identifier = $_POST["identifier"]; // Kullanıcı adı veya E-posta
+    $password = $_POST["password"];
+
+    // Form alanlarının doğrulaması
+    if (empty($identifier) || empty($password)) {
+        die("Eksik giriş bilgileri.");
+    }
+
+    // Check if the identifier is a valid email format
+    if (filter_var($identifier, FILTER_VALIDATE_EMAIL)) {
+        $query = "SELECT * FROM users WHERE email = ?";
+    } else {
+        $query = "SELECT * FROM users WHERE username = ?";
+    }
+
+    try {
+        $stmt = $db->prepare($query);
+        $stmt->execute([$identifier]);
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($user && password_verify($password, $user["password"])) {
+            // Kullanıcı girişi başarılı, oturum başlat
+            $_SESSION["user_id"] = $user["id"];
+            $_SESSION["user_type"] = $user["user_type"];
+
+            // Kullanıcı türüne göre yönlendirme yap
+            header("Location: user_panel.php");
+            exit();
+        } else {
+            // Kullanıcı doğrulanamadı, hata mesajı gösterme
+            echo '<div class="alert alert-danger" role="alert">
+                Hatalı e-posta adresi ya da şifre girdiniz.
+            </div>';
+        }
+    } catch (PDOException $e) {
+        echo "Hata: " . $e->getMessage();
+    }
+    // Veritabanı bağlantısını kapat
+    $db = null;
 }
+?>
 
-$csrf_token = $_SESSION['csrf_token'];
-require_once "header.php";
-?>
+<?php require_once "header.php"; ?>
+
 <main class="form-signin w-100 m-auto">
-        <form method="post" action="user_login_process.php">
-            <input type="hidden" name="csrf_token" value="<?php echo $csrf_token; ?>">
-            <img class="mb-4" src="./assets/brand/default_logo.png" alt="<?php echo $siteName ?>" title="<?php echo $siteName ?>" width="100" height="100">
-            <h1 class="h3 mb-3 fw-normal">Kullanıcı Paneli</h1>
-            <div class="form-floating">
-                <input type="text" class="form-control" id="identifier" name="identifier" placeholder="E-posta / Kullanıcı adı" autofocus="" required>
-                <label for="floatingInput">E-posta / Kullanıcı adı</label>
-            </div>
-            <div class="form-floating">
-                <input type="password" class="form-control" name="password" id="password" placeholder="Şifre" required>
-                <label for="floatingPassword">Şifre</label>
-                <div class="form-group mt-3">
-                    <a class="text-light-emphasis text-decoration-none" href="reset_password.php">Şifremi unuttum</a>
-                </div>
-            </div>
+    <form method="post" action="user_login.php">
+        <input type="hidden" name="csrf_request" value="1">
+
+        <img class="mb-4" src="./assets/brand/default_logo.png" alt="<?php echo $siteName ?>" title="<?php echo $siteName ?>" width="100" height="100">
+        <h1 class="h3 mb-3 fw-normal">Kullanıcı Paneli</h1>
+        <div class="form-floating">
+            <input type="text" class="form-control" id="identifier" name="identifier" placeholder="E-posta / Kullanıcı adı" autofocus="" required>
+            <label for="floatingInput">E-posta / Kullanıcı adı</label>
+        </div>
+        <div class="form-floating">
+            <input type="password" class="form-control" name="password" id="password" placeholder="Şifre" required>
+            <label for="floatingPassword">Şifre</label>
             <div class="form-group mt-3">
-                <button class="btn btn-primary w-100 py-2" type="submit">
-                    <i class="fas fa-sign-in-alt"></i> Oturum aç
-                </button>
-                <a href="<?php echo $siteUrl ?>" class="btn btn-secondary w-100 py-2 mt-2">
-                    <i class="fas fa-home"></i> <?php echo $siteName ?> - <?php echo $siteShortName ?>
-                </a>
+                <a class="text-light-emphasis text-decoration-none" href="reset_password.php">Şifremi unuttum</a>
             </div>
-        </form>
-    </main>
-</form>
-</div>
-<?php
-require_once "footer.php";
-?>
+        </div>
+        <div class="form-group mt-3">
+            <button class="btn btn-primary w-100 py-2" type="submit">
+                <i class="fas fa-sign-in-alt"></i> Oturum aç
+            </button>
+            <a href="<?php echo $siteUrl ?>" class="btn btn-secondary w-100 py-2 mt-2">
+                <i class="fas fa-home"></i> <?php echo $siteName ?> - <?php echo $siteShortName ?>
+            </a>
+        </div>
+    </form>
+</main>
+
+<?php require_once "footer.php"; ?>
