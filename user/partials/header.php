93b8091e0b8e7f9966f60b12a087871de59514bb
Muhammed Yalçınkaya <55546452+muhyal@users.noreply.github.com>
2024-02-06 22:52:45 +0300

    küçük hata giderme

--- user/partials/header.php
+++ user/partials/header.php
@@ -276,7 +276,7 @@ logoutUser();
     </symbol>
 </svg>
 
-<  <div class="dropdown position-fixed bottom-0 end-0 mb-3 me-3 bd-mode-toggle">
+  <div class="dropdown position-fixed bottom-0 end-0 mb-3 me-3 bd-mode-toggle">
     <button class="btn btn-bd-primary py-2 dropdown-toggle d-flex align-items-center"
             id="bd-theme"
             type="button"
